15a7528c07dcc9d2bcb9030214795059b60ccb94
Thiago Souza <thiagocss123@gmail.com>
2025-06-03 22:00:35 -0300

    FIX: http exception

--- core/user/use_cases/find_user.py
+++ core/user/use_cases/find_user.py
@@ -1,13 +1,17 @@
 from app.User import User
+from fastapi import HTTPException
+from starlette.status import HTTP_404_NOT_FOUND
 
 
 class FindUser:
     def execute(self, user_id: int) -> User:
         print(user_id)
-        user = User.find(user_id).serialize()
-        print(user)
+        user = User.find(user_id)
 
         if not user:
-            raise Exception(f"Usuário com ID {user_id} não encontrado.")
+            raise HTTPException(
+                status_code=HTTP_404_NOT_FOUND,
+                detail=f"Usuário com ID {user_id} não encontrado."
+            )
 
-        return user
+        return user.serialize()
