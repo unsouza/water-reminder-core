81004986c2fd68c6037bdff1dbf34fa0573a2fb9
Thiago Souza <thiagocss123@gmail.com>
2025-06-05 19:11:54 -0300

    add configuration of the env example

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,8 @@ set -e
 echo "==> Intalando dependências ..."
 pip install --no-cache-dir -r requirements.txt
 
+echo "=> Configurando variavel ambiente ..."
+cp .env.example .env
+
 echo "==> Iniciando servidor FastAPI..."
 exec fastapi dev run.py --host 0.0.0.0 --port 8000
